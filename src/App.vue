8d9666fc231bb62d9d60bac4092531f4c57f8035
ikayani <ismailkayani@teresol.com>
2024-08-22 17:20:19 +0500

    first commit of vue

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import MainCards from '@/components/MainCards.vue';
+</script>
+<template>
+  <Navbar />
+  <Hero />
+  <MainCards />
+</template>
